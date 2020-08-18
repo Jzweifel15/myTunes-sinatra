b95a66ea3e73e0abbea9682a65c6e5da9ed7db5c
Jzweifel15 <jzweifel15@yahoo.com>
2020-08-18 17:04:05 -0400

    Bug Fixes
    Spelled 'password_digest' wrong in the original Users migration file; now spelled correctly

--- db/schema.rb
+++ db/schema.rb
@@ -27,7 +27,7 @@ ActiveRecord::Schema.define(version: 2020_08_18_175742) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
-    t.string "password_disgest"
+    t.string "password_digest"
     t.integer "playlist_id"
   end
 
