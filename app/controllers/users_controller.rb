385ef2c2f5b266b3f18bcc119122c50b17ead39c
Jzweifel15 <jzweifel15@yahoo.com>
2020-08-18 15:43:30 -0400

    Helper Methods Created
    Created helper methods that checks if the user is logged in or if the user is the current user of the session

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,18 @@ class UsersController < ApplicationController
     end
 
     get '/index' do 
+
+    end
+
+    helpers do 
+
+        def logged_in?
+            !!session[:user_id]
+        end
+
+        def current_user
+            User.find(session[:user_id])
+        end
         
     end
 
