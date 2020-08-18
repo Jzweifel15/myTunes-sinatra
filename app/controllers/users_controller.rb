4570b68975b850047627a402da4b897d1add9073
Jzweifel15 <jzweifel15@yahoo.com>
2020-08-18 15:39:26 -0400

    Password Authentication
    Making sure a user signs in with the correct usernam and password

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,19 @@ class UsersController < ApplicationController
         erb :'/users/signin'
     end
 
+    post '/signin' do 
+        user = User.find_by(username: params[:username], password: params[:password])
+
+        if user && user.authenticate(params[:password])
+            session[:user_id] = user.id 
+            redirect '/index'
+        else
+            redirect '/signin'
+        end
+    end
+
+    get '/index' do 
+        
+    end
+
 end
